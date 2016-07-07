00cd64f2e5621d832ef432f3ef7bab6576f4d4bf
Training <Training@DIN39000537.corp.capgemini.com>
2016-07-07 10:01:42 +0530

    Initial commit 10:01

---
+++ Product/src/org/cap/demo/ProductApp.java
@@ -0,0 +1,8 @@
+package org.cap.demo;
+
+public class ProductApp {
+
+	public void display(){
+		System.out.println("from product");
+	}
+}
---
+++ Product/src/org/cap/demo/Productmain.java
@@ -0,0 +1,12 @@
+package org.cap.demo;
+
+public class Productmain {
+public static void main(String[] args) {
+	ProductApp app = new ProductApp();
+	app.display();
+	greetings();
+}
+public static void greetings(){
+	System.out.println("from main");
+}
+}
