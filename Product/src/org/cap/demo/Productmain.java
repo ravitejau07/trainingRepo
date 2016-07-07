b476c8e44492e33074bc97465bc6fbbb41d03495
Training <Training@DIN39000537.corp.capgemini.com>
2016-07-07 10:10:10 +0530

    secong commmmit

--- Product/src/org/cap/demo/Productmain.java
+++ Product/src/org/cap/demo/Productmain.java
@@ -9,5 +9,6 @@ public static void main(String[] args) {
 public static void greetings(){
 	System.out.println("from main");
 	System.out.println("first change");
+	System.out.println("secong change");
 }
 }
